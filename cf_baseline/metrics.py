739413be70668e81ca2f4c0f39d358c74df38f5e
nutorbit <nutorbitx@gmail.com>
2022-11-23 20:37:38 +0700

    fix bug

--- cf_baseline/metrics.py
+++ cf_baseline/metrics.py
@@ -2,32 +2,27 @@ import torch
 
 
 def calculate_mrr(
-    predictions: torch.Tensor,
-    targets: torch.Tensor,
+    predicted_items: torch.Tensor,
+    target_item: torch.Tensor,
     reduction: str = "mean",
 ) -> torch.Tensor:
     """
     Calculate the Mean Reciprocal Rank (MRR) metric.
 
     Args:
-        predictions: The predictions, (batch_size, n_items).
-        targets: The targets, (batch_size, n_items).
-        reduction: The reduction method. Defaults to "mean".
-
+        predicted_items: The predictions, (batch_size, n_items).
+        target_item: The ground truth, (batch_size, )
+        
     Returns:
-        The MRR metric.
+        MRR score
     """
-    
-    # Get the ranks of the targets
-    ranks = (targets == predictions[:, 0].unsqueeze(1)).nonzero()[:, 1]
 
-    # Calculate the MRR
-    mrr = 1 / (ranks + 1).float()
+    rank = (predicted_items == target_item.unsqueeze(1)).nonzero()[:, 1] + 1
+    mrr = 1.0 / rank.float()
 
-    # Reduce the MRR
     if reduction == "mean":
-        mrr = mrr.mean()
+        return mrr.mean()
     elif reduction == "sum":
-        mrr = mrr.sum()
+        return mrr.sum()
 
     return mrr
