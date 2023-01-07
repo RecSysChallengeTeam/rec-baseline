8e227ab61b4cea401411ac17a799d6630f75d2d6
nutorbit <nutorbitx@gmail.com>
2023-01-07 14:22:40 +0700

    update common

--- cf_baseline/common.py
+++ cf_baseline/common.py
@@ -4,15 +4,16 @@ import torch
 def get_device() -> str:
     """
     Returns the device that available for use. 
-    Priority: M1 (MPS) > GPU (CUDA) > CPU
+    Priority:  GPU (CUDA) > M1 (MPS) > CPU
     
     Returns:
         string of device name
     """
     
+    if torch.cuda.is_available():
+        return "cuda"
     if hasattr(torch.backends, "mps"):
         if torch.backends.mps.is_available() and torch.backends.mps.is_built():
             return "mps"
-    if torch.cuda.is_available():
-        return "cuda"
     return "cpu"
+
